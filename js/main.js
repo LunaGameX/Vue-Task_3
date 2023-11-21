b8d3058bea26e0e77f9a08bce7abf69e3977b376
LunaGameX <>
2023-11-21 18:02:41 +0300

    update projects: realizated modal-window and modal-form

--- js/main.js
+++ js/main.js
@@ -55,6 +55,69 @@ Vue.component('cards-kanban', {
     }
 })
 
+Vue.component('fill', {    //дата создания, заголовок, описание задачи, дедлайн
+    template: `
+    <div xmlns="http://www.w3.org/1999/html">
+    <div>
+        <button v-if="!show" @click="openModal" id="buttonModal">Добавьте задачу</button>
+        <div id="form" v-if="show" class="modal-shadow">
+            <div class="modal">
+                <div class="modal-close" @click="closeModal">&#10006;</div>
+                <h3>Заполните карточку задачи</h3>
+                <form @submit.prevent="onSubmit">
+                    <p class="pForm">Введите заголовок: </br>
+                        <input required type="text" v-model="title" maxlength="30" placeholder="Заголовок">
+                    </p>
+                    <p class="pForm">Добавьте описание задаче: </p>
+                    <textarea v-model="description" wrap="hard" cols="40" rows="4"></textarea>
+                    <p class="pForm">Укажите дату дедлайна: </br>
+                        <input required type="date" v-model="dateD">
+                    </p>
+                    <p class="pForm">
+                        <input class="button" type="submit" value="Добавить задачу">
+                    </p>
+                </form>
+            </div>
+        </div>    
+    </div>
+    `,
+    data() {
+        return {
+            title: null,
+            description: null,
+            dateD: null,
+            show: false
+        }
+    },
+    methods: {
+        onSubmit() {
+            let card = {
+                title: this.title,
+                description: this.description,
+                dateD: this.dateD,                     //дата дедлайна
+                dateC: new Date().toLocaleString(),   //дата создания
+                updateCard: false,
+                dateL: null,                            //дата последних изменений
+                dateE: null,                            //дата выполнения
+                inTime: true,                           //в срок или нет
+                reason: []
+            }
+            eventBus.$emit('card-create', card)
+            this.title = null
+            this.description = null
+            this.dateD = null
+            this.closeModal()
+            console.log(card)
+        },
+        closeModal() {
+            this.show = false
+        },
+        openModal() {
+            this.show = true
+        }
+    }
+})
+
 let app = new Vue({
     el: '#app',
     data: {}
